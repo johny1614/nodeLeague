40dccc554143f27e29151a305221ec5c83d2861a
Michał Lis <michal.lis@softwareplant.com>
2021-04-02 22:44:37 +0200

    have ts but without @types/node

--- src/helloWorldServer.ts
+++ src/helloWorldServer.ts
@@ -1,5 +1,6 @@
-const http = require('http');
+import * as http from 'http';
+
 http.createServer((req, res) => {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
 	res.end('hello world\n');
 }).listen(8000, '127.0.0.1');
